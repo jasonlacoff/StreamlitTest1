f59d3d1e1d747311df93e725cc34c1cbff1d9ebd
jasonlacoff <111462312+jasonlacoff@users.noreply.github.com>
2022-08-17 16:31:25 -0500

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -2,3 +2,11 @@ import streamlit as st
 
 st.title('test')
 
+df = pd.DataFrame(
+    np.random.randn(50, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(df)  # Same as st.write(df)
+
+
+st.dataframe(df, 200, 100)
